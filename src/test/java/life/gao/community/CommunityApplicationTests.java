8a8e561fe7a7babb62d4d9600b76008f47e159df
Gaoyan1999 <1803483451@qq.com>
2020-04-01 23:41:26 +0800

    init

---
+++ src/test/java/life/gao/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.gao.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
