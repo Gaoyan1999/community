6bc716f7936932f1e02495d0e3baa1aa92fd958a
Gaoyan1999 <1803483451@qq.com>
2020-04-24 18:37:58 +0800

    修改user表

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,15 +1,13 @@
-create table QUESTION
+create table USER
 (
-    ID            BIGINT auto_increment not null,
-    TITLE         VARCHAR(50)  not null,
-    DESCRIPTION   CLOB         not null,
-    GMT_CREATE    BIGINT  default 0,
-    GMT_MODIFIED  BIGINT  default 0,
-    CREATOR       BIGINT,
-    COMMENT_COUNT INTEGER default 0,
-    VIEW_COUNT    INTEGER default 0,
-    LIKE_COUNT    INTEGER default 0,
-    TAG           VARCHAR(256) not null,
-    constraint QUESTION_PK
+    ID            BIGINT auto_increment  not null,
+    ACCOUNT_ID   VARCHAR(100) not null,
+    NAME         VARCHAR(50),
+    TOKEN        VARCHAR(36),
+    GMT_CREATE   BIGINT       not null,
+    GMT_MODIFIED BIGINT       not null,
+    BIO          VARCHAR(256),
+    AVATAR_URL   VARCHAR(100),
+    constraint USER_PK
         primary key (ID)
 );
